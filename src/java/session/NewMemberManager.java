3d6ff86738d50dd8b172a0243ba26713fe7c1eaf
jtony123 <jtony123@gmail.com>
2015-01-08 12:30:49 +0000

    update 08012015

---
+++ src/java/session/NewMemberManager.java
@@ -0,0 +1,19 @@
+/*
+ * No license found
+ * 
+ * 
+ */
+package session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author jtony_000
+ */
+@Stateless
+public class NewMemberManager {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
